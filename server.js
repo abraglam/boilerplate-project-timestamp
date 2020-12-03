de8f286a6328ab0638f8e6850005e5c182a8fa84
Daniel Wilson <stringofbeans@gmail.com>
2020-12-03 13:05:21 -0800

    first commit

--- server.js
+++ server.js
@@ -4,15 +4,14 @@
 // init project
 var express = require('express');
 var app = express();
-
+var bodyParser = require('body-parser');
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
-
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
-
+app.use(bodyParser.urlencoded({extended:false}));
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
@@ -21,12 +20,30 @@ app.get("/", function (req, res) {
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({greeting: 'hello API', frustrated: true});
 });
 
+app.get('/api/timestamp', (req, res)=>{
+  const date = new Date();
+  res.json({unix: Date.parse(date), utc: date.toUTCString()});
+});
 
+app.get("/api/timestamp/:date", function (req, res) {
+  let date = new Date(req.params.date);
+  if(isNaN(Date.parse(date))){
+    date = new Date(parseInt(req.params.date))
+      if(isNaN(Date.parse(date))){
+        res.json({error: "Invalid Date"});
+      } else {
+        res.json({unix: Date.parse(date), utc: date.toUTCString()});
+      }
+  } else {
+    res.json({unix: Date.parse(date), utc: date.toUTCString()});
+  }
+  
+});
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
